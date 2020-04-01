58eeb99c9a9f5cb53f177e31f99a687b39198398
macocz <maciek.oczos@gmail.com>
2020-04-01 21:01:07 +0200

    UITableView

--- PrzepisyKulinarne/RecipesList/View/RecipesViewController .swift
+++ PrzepisyKulinarne/RecipesList/View/RecipesViewController .swift
@@ -8,11 +8,51 @@
 
 import UIKit
 
-class RecipesViewController : UIViewController {
+class RecipesViewController: UIViewController {
+    private let tableView = UITableView()
+    private let emptyLabel = UILabel()
+    private let recipies = ["Donut", "Pizza"] //tablice stringow
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .white
+        setupTableView()
+        setupEmptyLabel()
+        setupFullLabel()
         
+        
+    }
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        tableView.backgroundColor = .white
+        tableView.separatorStyle = .none
+    }
+    
+    private func setupEmptyLabel() {
+        
+        tableView.backgroundView = emptyLabel
+        emptyLabel.text = "There is no data available"
+        emptyLabel.textAlignment = .center
+        
+    }
+    
+    private func setupFullLabel() {
+        
+        
+        
+    
     }
     
     
